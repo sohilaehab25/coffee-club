d086389798ccee14d18109d78fb9c68ffaca0a22
sohilaehab25 <sohilaehab25@gmail.com>
2025-08-18 23:04:23 +0300

    MAIN-1 add item apies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryModule } from './category/category.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
   imports: [
     AuthModule,
     MongooseModule.forRoot('mongodb://localhost:27017/coffee-club'),
-    CategoryModule, 
+    CategoryModule,
+    ItemModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
